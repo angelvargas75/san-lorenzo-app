e14f77a00956007fa96d1322d927f4f0fd461be9
Angel Vargas <angelsk814@gmail.com>
2026-06-23 02:49:34 -0500

    feat: configura routing base y router-outlet principal

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,46 @@
 import { Routes } from '@angular/router';
+import { Login } from './features/auth/login/login';
+import { AlumnoLayout } from './features/alumno/alumno-layout/alumno-layout';
+import { DocenteLayout } from './features/docente/docente-layout/docente-layout';
+import { CoordinadorLayout } from './features/coordinador/coordinador-layout/coordinador-layout';
+import { Inicio } from './features/alumno/inicio/inicio';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  // Al entrar, redirige al login
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+
+  // Ruta del login
+  { path: 'login', component: Login },
+
+  // Ruta del alumno (el layout es el padre, los hijos los agregará Dev 2)
+  {
+    path: 'alumno',
+    component: AlumnoLayout,
+    children: [
+         { path: '', redirectTo: 'inicio', pathMatch: 'full' },
+        { path: 'inicio', component: Inicio },
+      // Dev 2 agregará aquí: inicio, calificaciones, tareas, etc.
+    ]
+  },
+
+  // Ruta del docente (Dev 3 agregará los hijos)
+  {
+    path: 'docente',
+    component: DocenteLayout,
+    children: [
+      // Dev 3 agregará aquí sus secciones
+    ]
+  },
+
+  // Ruta del coordinador (Dev 4 y 5 agregarán los hijos)
+  {
+    path: 'coordinador',
+    component: CoordinadorLayout,
+    children: [
+      // Dev 4 y 5 agregarán aquí sus secciones
+    ]
+  },
+
+  // Si la ruta no existe, redirige al login
+  { path: '**', redirectTo: 'login' }
+];
