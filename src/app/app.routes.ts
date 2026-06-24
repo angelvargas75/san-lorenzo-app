06f59a69b269c25da3de5e39c4f0bcbb36697570
Victor Ortiz Ortiz <SOPTEC19@comercio.com.pe>
2026-06-24 14:08:08 -0500

    feat: implementa vista de usuarios

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,46 +5,36 @@ import { DocenteLayout } from './features/docente/docente-layout/docente-layout'
 import { CoordinadorLayout } from './features/coordinador/coordinador-layout/coordinador-layout';
 import { Inicio } from './features/alumno/inicio/inicio';
 import { authGuard } from './core/guards/auth-guard';
+import { Usuarios } from './features/coordinador/usuarios/usuarios';
+
 
 export const routes: Routes = [
-  // Al entrar, redirige al login
   { path: '', redirectTo: 'login', pathMatch: 'full' },
-
-  // Ruta del login
   { path: 'login', component: Login },
-
-  // Ruta del alumno (el layout es el padre, los hijos los agregará Dev 2)
   {
     path: 'alumno',
     component: AlumnoLayout,
     canActivate: [authGuard],
     children: [
-         { path: '', redirectTo: 'inicio', pathMatch: 'full' },
-        { path: 'inicio', component: Inicio },
-      // Dev 2 agregará aquí: inicio, calificaciones, tareas, etc.
+      { path: '', redirectTo: 'inicio', pathMatch: 'full' },
+      { path: 'inicio', component: Inicio }
     ]
   },
-
-  // Ruta del docente (Dev 3 agregará los hijos)
   {
     path: 'docente',
     component: DocenteLayout,
     canActivate: [authGuard],
-    children: [
-      // Dev 3 agregará aquí sus secciones
-    ]
+    children: []
   },
-
-  // Ruta del coordinador (Dev 4 y 5 agregarán los hijos)
   {
     path: 'coordinador',
     component: CoordinadorLayout,
     canActivate: [authGuard],
     children: [
-      // Dev 4 y 5 agregarán aquí sus secciones
+      { path: 'usuarios', component: Usuarios }
     ]
   },
-
-  // Si la ruta no existe, redirige al login
   { path: '**', redirectTo: 'login' }
 ];
+```
+eof
---
+++ src/app/features/coordinador/inicio-coordinador/inicio-coordinador.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-inicio-coordinador',
+  imports: [],
+  templateUrl: './inicio-coordinador.html',
+  styleUrl: './inicio-coordinador.scss',
+})
+export class InicioCoordinador {}
