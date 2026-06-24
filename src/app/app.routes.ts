cc17efe45dbf3428ee6c3d065f649ec9a0cfd657
bvargasg24-spec <bvargasg24@gmail.com>
2026-06-24 08:11:20 -0500

    feat: implementa rutas de las 5 secciones del alumno

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,47 +4,41 @@ import { AlumnoLayout } from './features/alumno/alumno-layout/alumno-layout';
 import { DocenteLayout } from './features/docente/docente-layout/docente-layout';
 import { CoordinadorLayout } from './features/coordinador/coordinador-layout/coordinador-layout';
 import { Inicio } from './features/alumno/inicio/inicio';
+import { Calificaciones } from './features/alumno/calificaciones/calificaciones';
+import { Tareas } from './features/alumno/tareas/tareas';
+import { Horarios } from './features/alumno/horarios/horarios';
+import { Asistencia } from './features/alumno/asistencia/asistencia';
+import { Perfil } from './features/alumno/perfil/perfil';
 import { authGuard } from './core/guards/auth-guard';
 
 export const routes: Routes = [
-  // Al entrar, redirige al login
   { path: '', redirectTo: 'login', pathMatch: 'full' },
-
-  // Ruta del login
   { path: 'login', component: Login },
-
-  // Ruta del alumno (el layout es el padre, los hijos los agregará Dev 2)
   {
     path: 'alumno',
     component: AlumnoLayout,
     canActivate: [authGuard],
     children: [
-         { path: '', redirectTo: 'inicio', pathMatch: 'full' },
-        { path: 'inicio', component: Inicio },
-      // Dev 2 agregará aquí: inicio, calificaciones, tareas, etc.
+      { path: '', redirectTo: 'inicio', pathMatch: 'full' },
+      { path: 'inicio', component: Inicio },
+      { path: 'calificaciones', component: Calificaciones },
+      { path: 'tareas', component: Tareas },
+      { path: 'horarios', component: Horarios },
+      { path: 'asistencia', component: Asistencia },
+      { path: 'perfil', component: Perfil },
     ]
   },
-
-  // Ruta del docente (Dev 3 agregará los hijos)
   {
     path: 'docente',
     component: DocenteLayout,
     canActivate: [authGuard],
-    children: [
-      // Dev 3 agregará aquí sus secciones
-    ]
+    children: []
   },
-
-  // Ruta del coordinador (Dev 4 y 5 agregarán los hijos)
   {
     path: 'coordinador',
     component: CoordinadorLayout,
     canActivate: [authGuard],
-    children: [
-      // Dev 4 y 5 agregarán aquí sus secciones
-    ]
+    children: []
   },
-
-  // Si la ruta no existe, redirige al login
   { path: '**', redirectTo: 'login' }
 ];
---
+++ src/app/features/alumno/tareas/tareas.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tareas',
+  imports: [],
+  templateUrl: './tareas.html',
+  styleUrl: './tareas.scss',
+})
+export class Tareas {}
